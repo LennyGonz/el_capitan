ebdfe0cfa9dc56de0ac5b217357b0917ffb2088c
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-18 01:01:41 -0400

    Added gatsby plugins I plan to use

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Lenny Gonzalez',
+    description: 'My Personal Website built to share my experiences, both personal and career.'
+  },
+  plugins: ['gatsby-plugin-react-helmet','gatsby-plugin-sass']
+}
