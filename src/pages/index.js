953b616592cd18906fa01f2dccc62d4c20ea2743
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-18 01:03:12 -0400

    refactored the index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'gatsby';
 
-export default function app() {
-  return <h1> Hello World! </h1>
+export default function index() {
+  return (
+    <div>
+      <h1> Hello World! </h1>
+      <p> Hello New York! </p>
+      <Link to='/about/'> Learn about me &rarr;</Link>
+    </div>
+  )
 }
