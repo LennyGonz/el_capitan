304819071617004905c95d8f867add7c130e795d
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-18 01:04:44 -0400

    adding a new directory and page

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Helmet from 'react-helmet'
+
+const Layout = ({ children }) => (
+  <Helmet>
+    <html lang="en">
+      <title>Temp</title>
+      <meta name='description' content='site description'></meta>
+    </html>
+  </Helmet>
+)
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <div>
+    <h1>About Me!</h1>
+    <p>This is my personal wbesite</p>
+    <Link to="/"> &larr; back to home </Link>
+  </div>
+)
