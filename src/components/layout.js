b9de4b20e2977d555d227b612425d017e77cbdbb
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-23 00:48:06 -0400

    Helmet set up to reflect added information

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
+import useSiteMetadata from '../hooks/use-sitemetadata';
 
-const Layout = ({ children }) => (
-  <Helmet>
-    <html lang="en">
-      <title>Temp</title>
-      <meta name='description' content='site description'></meta>
-    </html>
-  </Helmet>
-)
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name='description' content={description} />
+      </Helmet>
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+};
+
+export default Layout;
