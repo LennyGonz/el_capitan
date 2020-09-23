f3f153f92b8bb5c7ee6bee4529d181bc395e9d01
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-09-23 00:49:10 -0400

    added layout to compass the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function index() {
   return (
-    <div>
-      <h1> Hello World! </h1>
-      <p> Hello New York! </p>
-      <Link to='/about/'> Learn about me (my name is mario) &rarr;</Link>
-    </div>
+      <Layout>
+        <h1> Hello World! </h1>
+        <p> Hello New York! </p>
+        <Link to='/about/'> Learn about me (my name is mario) &rarr;</Link>
+      </Layout>
   )
 }
