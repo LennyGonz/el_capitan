4941ca57a951cf8d80a39dbb12192e9be55fc94b
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-01 01:18:48 -0400

    setting up the page

--- src/pages/photo-gallery.js
+++ src/pages/photo-gallery.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const photogallery = () => {
   return (
-    <h1> Photo Gallery </h1>
+    <div>
+      <h1> Photo Gallery </h1>
+      <div className="row">
+        <article className="col-1-2">
+          <h3>Photo 1</h3>
+          <img src="../img/andre-benz-lIa03ti94ec-unsplash.jpg" />
+        </article>
+        <article className="col-1-2">
+          <h3>Photo 2</h3>
+          <img src="../img/flint-blake-4T91C9wDAyI-unsplash.jpg" />
+        </article>
+        <article className="col-1-2">
+          <h3>Photo 3</h3>
+          <img src="../img/lachlan-gowen-Wzm6nQFPY94-unsplash.jpg" />
+        </article>
+        <article className="col-1-2">
+          <h3>Photo 4</h3>
+          <img src="../img/luke-white-jXGxY3Y7mpE-unsplash.jpg" />
+        </article>
+      </div>
+    </div>
   )
 }
 
