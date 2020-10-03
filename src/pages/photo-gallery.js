da14287c189d9d3845d8263b3ee78ba683183c51
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-02 22:21:30 -0400

    photos

--- src/pages/photo-gallery.js
+++ src/pages/photo-gallery.js
@@ -7,19 +7,23 @@ const photogallery = () => {
       <div className="row">
         <article className="col-1-2">
           <h3>Photo 1</h3>
-          <img src="../img/andre-benz-lIa03ti94ec-unsplash.jpg" />
+          <img src="../img/andre-benz-lIa03ti94ec-unsplash.jpg" alt="arb1" />
         </article>
         <article className="col-1-2">
           <h3>Photo 2</h3>
-          <img src="../img/flint-blake-4T91C9wDAyI-unsplash.jpg" />
+          <img src="../img/flint-blake-4T91C9wDAyI-unsplash.jpg" alt="arb2" />
         </article>
         <article className="col-1-2">
           <h3>Photo 3</h3>
-          <img src="../img/lachlan-gowen-Wzm6nQFPY94-unsplash.jpg" />
+          <img src="../img/lachlan-gowen-Wzm6nQFPY94-unsplash.jpg" alt="arb3" />
         </article>
         <article className="col-1-2">
           <h3>Photo 4</h3>
-          <img src="../img/luke-white-jXGxY3Y7mpE-unsplash.jpg" />
+          <img src="../img/luke-white-jXGxY3Y7mpE-unsplash.jpg" alt="arb4" />
+        </article>
+        <article>
+          <h3>Photo 5</h3>
+          <img src="../img/mary-ray-kTYzNElfk-0-unsplash.jpg" alt="arb5"></img>
         </article>
       </div>
     </div>
