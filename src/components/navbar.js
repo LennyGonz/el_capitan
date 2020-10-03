e73ce9d69c391171af085abcbfa7f9325354fd79
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-02 22:22:29 -0400

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const navBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
