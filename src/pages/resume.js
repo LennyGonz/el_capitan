2e19c29c0281a1880960ae10a38d732f4de87279
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-02 22:21:55 -0400

    arb

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Resume = () => {
-  const placeholder = '';
 
   return (
     <div>
       <h1> Resume Page </h1>
+      <p>  9505514367030274430149 </p>
     </div>
   )
 }
