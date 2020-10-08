0861a49ceaf1b4e1e078dd6bc48f84c4ab132e09
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-07 21:35:36 -0400

    Removed libraries no longer in use and added new ones

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: 'Lenny Gonzalez',
     description: 'My Personal Website built to share my experiences, both personal and career.'
   },
-  plugins: ['gatsby-plugin-react-helmet','gatsby-plugin-sass']
+  plugins: ['gatsby-plugin-react-helmet','gatsby-plugin-styled-components']
 }
