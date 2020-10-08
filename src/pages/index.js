8da6a94ce467c3fb27f376e17385af693ea5458d
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-07 21:44:37 -0400

    misc changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
-export default function index() {
+const Index = () => {
+
   return (
-      <Layout>
-        <h1> Hello World! </h1>
-        <p> Hello New York! </p>
-        <Link to='/about/'> Learn about me (my name is mario) &rarr;</Link>
-      </Layout>
+    <Layout>
+      <h1> Hello World! </h1>
+      <p> Hello New York! </p>
+      <Link to='/about/'> Learn about me (my name is mario) &rarr;</Link>
+    </Layout>
+    
   )
 }
+
+export default Index;
