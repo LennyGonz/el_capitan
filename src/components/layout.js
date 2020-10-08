74528903fa43bcb0c7ae1695130313a6a26a28d7
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-10-07 21:43:56 -0400

    added functionality for navbar in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react'
 import Helmet from 'react-helmet';
-import NavBar from './Navbar/NavBar';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 import useSiteMetadata from '../hooks/use-sitemetadata';
 import '../styles/App.css'
 
 const Layout = ({ children }) => {
   const { title, description } = useSiteMetadata();
+
+  const [isOpen, setIsOpen] = useState(false)
+  
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <div>
@@ -15,7 +22,8 @@ const Layout = ({ children }) => {
         <meta name='description' content={description} />
       </Helmet>
       <main>
-        <NavBar />
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <Navbar toggle={toggle} />
         {children}
       </main>
     </div>
